1931a976d7524b2014e32b8943c64f74270dc8cd
JJtrenado <f62trzaj@uco.es>
2023-07-04 21:50:24 +0200

    change extension

---
+++ src/modules/Login/Infraestructure/LocalStorageUser.ts
@@ -0,0 +1,12 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { User } from '../Domain/User';
+
+export async function saveLocalUser(user: User) {
+	await AsyncStorage.setItem("@user", JSON.stringify(user));
+}
+
+export async function getLocalUser(): Promise<User | null> { //devuelve un user o null
+	const data = await AsyncStorage.getItem("@user");
+	if (!data) return null;
+	return JSON.parse(data);
+}
