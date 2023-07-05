02ff1124654168fbec99bb95d91b70678ea57f1c
JJtrenado <f62trzaj@uco.es>
2023-07-05 17:39:31 +0200

    styling component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,6 @@
 import { WEB_GOOGLE_CLIENT_ID, ANDROID_GOOGLE_CLIENT_ID, IOS_GOOGLE_CLIENT_ID }from '@env';
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Button, Image } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
 import { removeLocalUser } from '../../modules/Login/Infraestructure/LocalStorageUser';
@@ -31,54 +31,70 @@ export default function Login() {
   return (
     <View style={styles.container}>
       {!userInfo ? (
-        <Button
-          title="Sign in with Google"
-          disabled={!request}
-          onPress={() => {
-            promptAsync();
-          }}
-        />
+        <TouchableOpacity style={styles.button}
+        onPress={() => {
+          promptAsync();
+        }}>
+        <Text style={styles.buttonText}>Iniciar con Google</Text>
+      </TouchableOpacity>
       ) : (
         <View style={styles.card}>
           {userInfo?.picture && (
             <Image source={{ uri: userInfo?.picture }} style={styles.image} />
             )}
-          <Text style={styles.text}>Email: {userInfo.email}</Text>
           <Text style={styles.text}>Name: {userInfo.name}</Text>
+          <Text style={styles.text}>Email: {userInfo.email}</Text>
         </View>
       )}
-      <Button
-        title="Logout"
+      
+      <TouchableOpacity style={styles.button}
         onPress={async () => {
         removeLocalUser();
         await reloadApp();
-        }}
-      />
+      }}>
+        <Text style={styles.buttonText}>Cerrar sesión</Text>
+      </TouchableOpacity>
     </View>
 
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#2196F3",
+    borderRadius: 5,
+    padding: 10,
+    margin: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
   text: {
     fontSize: 20,
+    margin: 5,
     fontWeight: "bold",
   },
   card: {
+    backgroundColor: "#e4e4e4",
+    borderColor: "#cacaca",
     borderWidth: 1,
-    borderRadius: 15,
-    padding: 15,
-    margin: 15,
+    borderRadius: 5,
+    padding: 10,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   image: {
     width: 100,
     height: 100,
     borderRadius: 50,
+    margin: 5,
   },
 });
