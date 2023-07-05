b5515cae1b523582967e3b836c94b35bb9886fae
JJtrenado <f62trzaj@uco.es>
2023-07-05 20:35:11 +0200

    JSDoc comment

--- src/modules/Login/Application/getLocalUserOrFetchFromGoogle.ts
+++ src/modules/Login/Application/getLocalUserOrFetchFromGoogle.ts
@@ -1,9 +1,16 @@
+import { AuthSessionResult } from "expo-auth-session";
 import { EndpointUser, User } from "../Domain/User";
 import { fetchUserInfoFromGoogle } from "../Infraestructure/fetchUserInfoFromGoogle";
 import { getLocalUser, saveLocalUser } from "../Infraestructure/LocalStorageUser";
 import { UserAdapter } from "./UserAdapter";
 
-export async function getLocalUserOrFetchFromGoogle(response) :Promise<User> {
+/**
+ * probes if the user is logged in(storaged in local), if not, fetches the user from google
+ *
+ * @param {AuthSessionResult} response response object obtained from the useAuthRequest hook provided by the Google API
+ * @return {Promise<User>} user from local storage or from google
+ */
+export async function getLocalUserOrFetchFromGoogle(response: AuthSessionResult) :Promise<User> {
     let user: User = await getLocalUser();
     if (!user) {
       if (response?.type === "success") {
