83338724a17ebf61ca021164e8b4ae6f692573f3
JJtrenado <f62trzaj@uco.es>
2023-07-05 17:35:04 +0200

    implements User not logged in error

--- src/modules/Login/Application/getLocalUserOrFetchFromGoogle.ts
+++ src/modules/Login/Application/getLocalUserOrFetchFromGoogle.ts
@@ -1,5 +1,5 @@
 import { EndpointUser, User } from "../Domain/User";
-import { getUserInfoFromGoogle } from "../Infraestructure/GoogleLogin";
+import { fetchUserInfoFromGoogle } from "../Infraestructure/fetchUserInfoFromGoogle";
 import { getLocalUser, saveLocalUser } from "../Infraestructure/LocalStorageUser";
 import { UserAdapter } from "./UserAdapter";
 
@@ -7,13 +7,13 @@ export async function getLocalUserOrFetchFromGoogle(response) :Promise<User> {
     let user: User = await getLocalUser();
     if (!user) {
       if (response?.type === "success") {
-        const endpointUser: EndpointUser = await getUserInfoFromGoogle(response.authentication.accessToken);
+        const endpointUser: EndpointUser = await fetchUserInfoFromGoogle(response.authentication.accessToken);
         user = UserAdapter(endpointUser);
         saveLocalUser(user);
       }
-    }
-    else {
-      console.log("loaded locally");
+      else {
+        throw new Error("User not logged in");
+      }
     }
     return user;
   }
