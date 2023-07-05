550b04c5b9226d57c6bd7969346818bda87eaa06
JJtrenado <f62trzaj@uco.es>
2023-07-05 17:35:52 +0200

    simplify code

--- App.js
+++ App.js
@@ -1,22 +1,7 @@
-import { StyleSheet, View } from "react-native";
 import Login from './src/components/Login/Login';
 
 export default function App() { 
   return (
-    <View style={styles.container}>
-      <Login/>
-    </View>
+    <Login/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    borderWidth: 1,
-    borderRadius: 15,
-    padding: 15,
-  }
-});
