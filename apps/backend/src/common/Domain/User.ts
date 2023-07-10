a5c1825c957ed6b84ed8be47b887a2debae987ea
JJtrenado <f62trzaj@uco.es>
2023-07-10 12:23:38 +0200

    delete unused endpoint user interface

--- apps/backend/src/common/Domain/User.ts
+++ apps/backend/src/common/Domain/User.ts
@@ -3,14 +3,3 @@ export interface User {
   email: string;
   token: string;
 }
-
-export interface EndpointUser {
-  id: string;
-  email: string;
-  verified_email: boolean;
-  name: string;
-  given_name: string;
-  family_name: string;
-  picture: string;
-  locale: string;
-}
