57d282d4f0b9c2320cdb20dcd94e7e17f719279f
JJtrenado <f62trzaj@uco.es>
2023-07-12 16:53:39 +0200

    adding a theme

---
+++ apps/client/src/components/theme.ts
@@ -0,0 +1,23 @@
+const theme = {
+  colors: {
+    primary: '#24292e',
+    white: '#fefefe',
+    textPrimary: '#24292e',
+    textSecondary: '#fefefe',
+    buttonPrimary: '#24292e',
+    buttonSecondary: '#fefefe',
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16
+  },
+  fonts: {
+    main: 'System'
+  },
+  fontWeights: {
+    normal: '400',
+    bold: '700'
+  }
+}
+
+export default theme
