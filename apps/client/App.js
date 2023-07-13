61f6126363b06aa788066ed602be2630b429d490
JJtrenado <f62trzaj@uco.es>
2023-07-13 16:29:38 +0200

    new screens

--- apps/client/App.js
+++ apps/client/App.js
@@ -6,6 +6,8 @@ import HomeScreen from "./src/components/pages/HomeScreen";
 import LoginScreen from "./src/components/pages/LoginScreen";
 import SettingsScreen from "./src/components/pages/SettingsScreen";
 import ScannScreen from "./src/components/pages/ScannScreen";
+import NewGarmentScreen from "./src/components/pages/NewGarmentScreen";
+import NewOutfitScreen from './src/components/pages/NewOutfitScreen';
 
 const Stack = createStackNavigator();
 
@@ -17,6 +19,8 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Settings" component={SettingsScreen} />
         <Stack.Screen name="Scann" component={ScannScreen} />
+        <Stack.Screen name="NewGarment" component={NewGarmentScreen} />
+        <Stack.Screen name="NewOutfit" component={NewOutfitScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
