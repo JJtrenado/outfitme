19194a4637990eb970b5283bfbb33146dbd2eae6
JJtrenado <f62trzaj@uco.es>
2023-07-13 16:29:28 +0200

    remove unused imports

--- apps/client/src/components/pages/LoginScreen.tsx
+++ apps/client/src/components/pages/LoginScreen.tsx
@@ -1,20 +1,18 @@
 // @ts-ignore
 import { WEB_GOOGLE_CLIENT_ID, ANDROID_GOOGLE_CLIENT_ID, IOS_GOOGLE_CLIENT_ID }from '@env';
 import React, { useEffect } from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
 import { User } from '../../modules/common/Domain/User';
 import { saveLocalUser } from '../../modules/common/Infrastructure/LocalStorageUser';
 import { getUserFromGoogle } from '../../modules/Login/Infrastructure/getUser';
 import { reloadApp } from '../../modules/common/Application/ReloadApp';
-import { useNavigation } from '@react-navigation/native';
 import StyledButton from '../atoms/StyledButton';
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function LoginScreen() {
-  const navigation = useNavigation();
 
   const [request, response, promptAsync] = Google.useAuthRequest({
     androidClientId: ANDROID_GOOGLE_CLIENT_ID,
