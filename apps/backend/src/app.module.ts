13864535a53045a1e3ff8cbdd5fb4a4e46b2076e
JJtrenado <f62trzaj@uco.es>
2023-07-18 20:53:27 +0200

    mongoose module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/Infrastructure/auth.module';
 import { BarCodeModule } from './barCode/Infrastructure/barCode.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, BarCodeModule],
+  imports: [
+    AuthModule,
+    BarCodeModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://trenadojuanjo:ksBQaehavYshWtPz@cluster0.vo0rone.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
 })
 export class AppModule {}
