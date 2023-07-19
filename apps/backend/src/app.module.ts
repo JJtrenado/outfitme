f98e784fe9967855f26805808a10aad40e233179
JJtrenado <f62trzaj@uco.es>
2023-07-19 13:18:51 +0200

    using env

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/Infrastructure/auth.module';
 import { BarCodeModule } from './barCode/Infrastructure/barCode.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { GarmentModule } from './garment/Infrastructure/garment.module';
 
 @Module({
   imports: [
     AuthModule,
     BarCodeModule,
+    GarmentModule,
     MongooseModule.forRoot(
-      'mongodb+srv://trenadojuanjo:ksBQaehavYshWtPz@cluster0.vo0rone.mongodb.net/?retryWrites=true&w=majority',
+      `mongodb+srv://trenadojuanjo:${process.env.DB_ADMIN_PASSWORD}@cluster0.vo0rone.mongodb.net/outfitme?retryWrites=true&w=majority`,
     ),
+    ,
   ],
 })
 export class AppModule {}
