0a3d27d450b4254e7617960a6b70023a5c47cded
JJtrenado <f62trzaj@uco.es>
2023-07-28 18:05:44 +0200

    basic styles

--- apps/client/src/components/molecules/GarmentView.tsx
+++ apps/client/src/components/molecules/GarmentView.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ActivityIndicator, FlatList } from 'react-native';
+import { View, Text, ActivityIndicator, FlatList, Image } from 'react-native';
 import { getGarmentByUser } from '../../modules/Garment/Infrastructure/getGarments';
 import { Garment } from '../../modules/Garment/Domain/garment';
+import { StyleSheet } from 'react-native';
 
-
-const GarmentListSimple = ( {jwt, userId} ) => {
+const GarmentListSimple = ({ jwt, userId }) => {
   const [garmentsData, setGarmentsData] = useState<Garment[]>([]);
   const [loading, setLoading] = useState(true);
 
@@ -19,9 +19,11 @@ const GarmentListSimple = ( {jwt, userId} ) => {
         setLoading(false);
       }
     };
-
+    
     fetchData();
   }, []);
+  
+  console.log('garmentsData', garmentsData);
 
   if (loading) {
     return (
@@ -33,19 +35,24 @@ const GarmentListSimple = ( {jwt, userId} ) => {
   }
 
   return (
-    <View>
+    <View style={styles.container}>
       <FlatList
         data={garmentsData}
         renderItem={({ item }) => (
-          <View>
-            <Text>User: {item.user}</Text>
-            <Text>BarCode: {item.barCode}</Text>
-            <Text>Type: {item.type}</Text>
-            <Text>Brand: {item.brand}</Text>
-            <Text>Model: {item.model}</Text>
-            <Text>Description: {item.description}</Text>
-            <Text>Available: {item.available ? 'Yes' : 'No'}</Text>
-            <Text>ImagePath: {item.imagePath}</Text>
+          <View style={styles.itemContainer}>
+            <View>
+              <Text>User: {item.user}</Text>
+              <Text>BarCode: {item.barCode}</Text>
+              <Text>Type: {item.type}</Text>
+              <Text>Brand: {item.brand}</Text>
+              <Text>Model: {item.model}</Text>
+              <Text>Description: {item.description}</Text>
+              <Text>Available: {item.available ? 'Yes' : 'No'}</Text>
+            </View>
+            <Image
+              source={{ uri: `http://192.168.1.19:3000/garments/${item.imagePath}` }}
+              style={styles.itemImage}
+            />
           </View>
         )}
         keyExtractor={(item) => item.barCode}
@@ -54,5 +61,22 @@ const GarmentListSimple = ( {jwt, userId} ) => {
   );
 };
 
-export default GarmentListSimple;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  itemContainer: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  itemImage: {
+    width: 100,
+    height: 100,
+  },
+});
 
+export default GarmentListSimple;
