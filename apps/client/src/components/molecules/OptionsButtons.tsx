ded42860703047c206daf006c1073f4fa09b4058
JJtrenado <f62trzaj@uco.es>
2023-12-24 10:07:55 +0100

    style: less margin for apk view

--- apps/client/src/components/molecules/OptionsButtons.tsx
+++ apps/client/src/components/molecules/OptionsButtons.tsx
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
   buttons: {
     justifyContent: "space-around",
     flexDirection: "row",
-    marginVertical: 20,
+    marginVertical: 10,
     alignItems: "center",
   },
 });
